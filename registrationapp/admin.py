9933f19b853993c3cc8d7a565a8941e0277f49ae
adamorujov <ademorujov@gmail.com>
2025-02-03 03:08:08 +0400

    ExtraPaymentModel added admin.py

--- registrationapp/admin.py
+++ registrationapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from registrationapp.models import (ClientModel, PaymentModel, SellerModel, 
 RegistrationModel, InstallmentInfoModel, InstallmentModel,
-FilterChangerModel, ChangeFilterModel, ServicerModel, ShuttleServiceModel
+FilterChangerModel, ChangeFilterModel, ServicerModel, ShuttleServiceModel, ExtraPaymentModel
 )
 
 
@@ -11,6 +11,7 @@ admin.site.register(SellerModel)
 admin.site.register(RegistrationModel)
 admin.site.register(InstallmentInfoModel)
 admin.site.register(InstallmentModel)
+admin.site.register(ExtraPaymentModel)
 admin.site.register(FilterChangerModel)
 admin.site.register(ChangeFilterModel)
 admin.site.register(ServicerModel)
