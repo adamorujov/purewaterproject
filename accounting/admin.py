3a64785e1c0f233d2d6684b80ffe97cc659bb774
adamorujov <ademorujov@gmail.com>
2025-02-03 03:09:14 +0400

    PersonaDailyPaymentModel added admin.py

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from accounting.models import DailyPaymentModel
+from accounting.models import DailyPaymentModel, PersonaDailyPaymentModel
 
 admin.site.register(DailyPaymentModel)
+admin.site.register(PersonaDailyPaymentModel)
