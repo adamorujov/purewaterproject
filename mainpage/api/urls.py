aaa4e4a5b0ac7c3ceea79e2fb61467e1ee94c716
adamorujov <ademorujov@gmail.com>
2025-02-26 04:56:50 +0400

    category and ourproduct api urls added.

--- mainpage/api/urls.py
+++ mainpage/api/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("settings-list/", views.SettingsListAPIView.as_view(), name="settings-list"),
     path("socialmedia-list/", views.SocialMediaListAPIView.as_view(), name="socialmedia-list"),
     path("service-list/", views.ServiceListAPIView.as_view(), name="service-list"),
+    path("category-list/", views.CategoryListAPIView.as_view(), name="category-list"),
+    path("category-ourproduct-list/<int:id>/", views.CategoryOurProductListAPIView.as_view(), name="category-ourproduct-list"),
     path("testimonial-list/", views.TestimonialListAPIView.as_view(), name="testimonial-list"),
 ]
